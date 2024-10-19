c7202388b135744d2efffbe24a17ec33fa085259
Garry028 <gmisal2002@gmail.com>
2024-10-19 17:52:52 +0530

    feat: added fonts

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -3,36 +3,38 @@ import ReactPlayer from "react-player";
 const MainSection = () => {
   return (
     <div className="bg-gradient-to-r from-primary to-tertiary text-neutral">
-    <section className=" py-16 px-4 lg:px-8 flex flex-col lg:flex-row justify-between items-center container mx-auto ">
-      <div className="max-w-md space-y-6 mb-8 lg:mb-0">
-        <h2 className="text-3xl lg:text-4xl font-bold">
-          97% Uptime? <br /> No Problem.
-        </h2>
-        <p className="text-lg lg:text-xl">
-          Reliability as a Service: Hassle-free support for EV Charging Stations
-        </p>
-        <p className="text-neutral text-base lg:text-lg">
-          Reliability as a Service (RaaS) is a labor subscription service that
-          provides peace of mind to charging station owners and operators, who
-          require fast and reliable O&M services at consistent, hassle-free
-          prices.
-        </p>
-        <button className="bg-primary text-neutral py-3 px-6 rounded-lg shadow-md hover:bg-secondary transition hover:text-primary">
-          Get reliable support today
-        </button>
-      </div>
+      <section className=" py-16 px-4 lg:px-8 flex flex-col lg:flex-row justify-between items-center container mx-auto ">
+        <div className="max-w-md space-y-6 mb-8 lg:mb-0">
+          <h2 className="text-3xl lg:text-4xl font-bold">
+            97% Uptime? <br /> No Problem.
+          </h2>
+          <p className="text-lg lg:text-xl">
+            Reliability as a Service: Hassle-free support for EV Charging
+            Stations
+          </p>
+          <p className="text-neutral text-base lg:text-lg">
+            Reliability as a Service (RaaS) is a labor subscription service that
+            provides peace of mind to charging station owners and operators, who
+            require fast and reliable O&M services at consistent, hassle-free
+            prices.
+          </p>
+          <button className="bg-primary text-neutral py-3 px-6 rounded-lg shadow-md hover:bg-secondary transition hover:text-primary">
+            Get reliable support today
+          </button>
+        </div>
 
-      <div className="w-full lg:w-1/2">
-        <ReactPlayer
-          url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-          className="react-player w-full h-64 rounded-lg shadow-lg"
-          controls={true}
-          playing={true} // This enables autoplay
-          width="100%"
-          height="100%"
-        />
-      </div>
-    </section></div>
+        <div className="w-full lg:w-1/2">
+          <ReactPlayer
+            url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+            className="react-player w-full h-64 rounded-lg shadow-lg"
+            controls={true}
+            playing
+            width="100%"
+            height="100%"
+          />
+        </div>
+      </section>
+    </div>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import BenefitsSection from "./components/BenefitsSection.jsx";
+import Footer from "./components/Footer.jsx";
 import Header from "./components/Header.jsx";
 import MainSection from "./components/MainSection.jsx";
 import "./index.css";
-import BenefitsSection from "./components/BenefitsSection.jsx";
-import Footer from "./components/Footer.jsx";
 
 createRoot(document.getElementById("root")).render(
   <>
@@ -12,7 +12,7 @@ createRoot(document.getElementById("root")).render(
       <Header />
       <MainSection />
       <BenefitsSection />
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   </>
 );
